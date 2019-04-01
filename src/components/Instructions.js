2cf9e3feacef69bfa1598cb074d9e98cf2b8ff20
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2019-04-01 18:08:39 +0200

    code clean up

--- src/components/Instructions.js
+++
@@ -1,9 +0,0 @@
-const Instructions = ({title, steps}) =>
-    <section>
-        <h2>{title}</h2>
-        {
-            steps.map((step, key) => <p key={key}>{step}</p>)
-        }
-    </section>
-
-export default Instructions;
--- src/main.js
+++ src/main.js
@@ -1,12 +1,2 @@
 import React from 'react'
 import {render} from 'react-dom'
-///import Menu from './components/Menu'
-import Summary from './components/Summary'
-import data from '../data/recipes' 
-
-window.React = React
-///<Menu recipes={data} />,
-render(
-  <Summary title="this is an over 20 characters title for this not to work"/>,
-  document.getElementById("react-container")
-)
